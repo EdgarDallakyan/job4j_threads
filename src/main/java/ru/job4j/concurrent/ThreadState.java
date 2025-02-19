f0d22254d46b2b58c47d69f2dd247046916618a2
edgar <edgar_dallakyan@mail.ru>
2025-02-19 14:03:31 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("Имя первой нити: " + Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println("Имя второй нити: " + Thread.currentThread().getName())
+        );
+        System.out.println("Имя главной нити: " + Thread.currentThread().getName());
+        System.out.println("Состояние первой нити ДО запуска метода start(): " + first.getState());
+        System.out.println("Состояние второй нити ДО запуска метода start(): " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Состояние первой нити ПОСЛЕ запуска метода start(): " + first.getState());
+            System.out.println("Состояние второй нити ПОСЛЕ запуска метода start(): " + second.getState());
+        }
+        System.out.println("Работа завершена!");
+    }
+}
